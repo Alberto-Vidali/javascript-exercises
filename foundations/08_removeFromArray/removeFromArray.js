ef2e9103122f1090dc6a247ddbf41ca9e57cdbc2
Alberto-Vidali <alby91v@gmail.com>
2026-07-14 18:00:12 +0200

    Done 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements) {
+        while (array.indexOf(element) >= 0) {
+            array.splice(array.indexOf(element),1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
