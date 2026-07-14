35530b5e3bc3f4c70360f18de8e0a5f4cc49016a
Alberto-Vidali <alby91v@gmail.com>
2026-07-14 16:48:24 +0200

    Done 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    else if (num === 0) return "";
+    else if (num === 1) return string;
+    const iteration = string;
+    for (let i = 2; i <= num; i++) {
+        string = string + iteration;
+    }
+    return string
 };
 
 // Do not edit below this line
