612e6c072f3c44765303d9ffae436399a2b0dd4f
Alberto-Vidali <alby91v@gmail.com>
2026-07-15 16:27:03 +0200

    Completed 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestAge = 0
+    let oldest;
+    for (const person of array) {
+        let age = 0;
+        if (person.yearOfDeath === undefined) {
+            let today = new Date()
+            age = today.getFullYear() - person.yearOfBirth;
+        }
+        else age = person.yearOfDeath - person.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
