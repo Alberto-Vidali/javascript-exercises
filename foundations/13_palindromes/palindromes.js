b2ad65a6864ae274564f46903ca04f31db324169
Alberto-Vidali <alby91v@gmail.com>
2026-07-15 14:49:09 +0200

    Completed 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    clearedString = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
+    .toLowerCase()
+    .replaceAll(" ","");
+    if (clearedString.length === 0 || clearedString.lenght === 1) return true;
+    for(let i = 0, j = clearedString.length - 1; i < clearedString.length; i++, j--) {
+        if (clearedString.charAt(i) != clearedString.charAt(j)) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
